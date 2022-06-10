9aa029d0036a059f9bd3067b98651b77f9a9ee58
withczh <withczh@qq.com>
2022-06-10 20:04:22 +0800

    server_ip

--- common.h
+++ common.h
@@ -3,8 +3,8 @@
 
 #define MAX_RECV_SEND_BUFF_SIZE 2048
 
-#define SERVER_PORT 8000
-#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8888
+#define SERVER_IP "127.0.0.1" //服务端ip地址，可修改
 
 #include <stdio.h>
 #include <memory.h>
@@ -12,6 +12,7 @@
 #include <assert.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include<arpa/inet.h>
 #include <netdb.h>
 
 #endif
